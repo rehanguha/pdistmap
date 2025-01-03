d633805aff3d4b3941b6f635d2d882e5cbddc4ed
Rehan Guha <rehanguha29@gmail.com>
2025-01-03 23:57:39 +0530

    Updated dependencies

--- pdistmap/set.py
+++ pdistmap/set.py
@@ -3,7 +3,6 @@ from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 from typing import Tuple
 
-
 class KDEIntersection:
     """
     A class used to compute the intersection area between two Kernel Density Estimations (KDEs).
@@ -219,6 +218,67 @@ class KDEIntersection:
         # Perform min-max scaling
         X_scaled = (data - X_min) / (X_max - X_min) * (max_val - min_val) + min_val
         return X_scaled
+    
+    def _trapezoid(self, y, x=None, dx=1.0, axis=-1):
+        """
+        Integrate along the given axis using the composite trapezoidal rule.
+
+        If `x` is provided, the integration happens in sequence along its
+        elements - they are not sorted.
+
+        Integrate `y` (`x`) along each 1d slice on the given axis, compute
+        :math:`\int y(x) dx`.
+        When `x` is specified, this integrates along the parametric curve,
+        computing :math:`\int_t y(t) dt =
+        \int_t y(t) \left.\frac{dx}{dt}\right|_{x=x(t)} dt`.
+
+        Parameters
+        ----------
+        y : array_like
+            Input array to integrate.
+        x : array_like, optional
+            The sample points corresponding to the `y` values. If `x` is None,
+            the sample points are assumed to be evenly spaced `dx` apart. The
+            default is None.
+        dx : scalar, optional
+            The spacing between sample points when `x` is None. The default is 1.
+        axis : int, optional
+            The axis along which to integrate.
+
+        Returns
+        -------
+        trapezoid : float or ndarray
+            Definite integral of `y` = n-dimensional array as approximated along
+            a single axis by the trapezoidal rule. If `y` is a 1-dimensional array,
+            then the result is a float. If `n` is greater than 1, then the result
+            is an `n`-1 dimensional array.
+        """
+        y = np.asanyarray(y)
+        if x is None:
+            d = dx
+        else:
+            x = np.asanyarray(x)
+            if x.ndim == 1:
+                d = np.diff(x)
+                # reshape to correct shape
+                shape = [1]*y.ndim
+                shape[axis] = d.shape[0]
+                d = d.reshape(shape)
+            else:
+                d = np.diff(x, axis=axis)
+        nd = y.ndim
+        slice1 = [slice(None)]*nd
+        slice2 = [slice(None)]*nd
+        slice1[axis] = slice(1, None)
+        slice2[axis] = slice(None, -1)
+        try:
+            ret = (d * (y[tuple(slice1)] + y[tuple(slice2)]) / 2.0).sum(axis)
+        except ValueError:
+            # Operations didn't work, cast to ndarray
+            d = np.asarray(d)
+            y = np.asarray(y)
+            ret = np.add.reduce(d * (y[tuple(slice1)]+y[tuple(slice2)])/2.0, axis)
+        return ret
 
     def _build_linespace(
         self, xmin: float, xmax: float, linespace_num: int = 10000
@@ -291,7 +351,7 @@ class KDEIntersection:
         self.kdeB_data = kdeB(self.data_linespace)
 
         self.inters = np.minimum(self.kdeA_data, self.kdeB_data)
-        self.area_inters = np.trapezoid(self.inters, self.data_linespace)
+        self.area_inters = self._trapezoid(self.inters, self.data_linespace)
         self.executed = True
 
         if plot:
